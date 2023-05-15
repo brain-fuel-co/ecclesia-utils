c31d53e1bd90dfd301503ddee970cf5c3c52e7d1
Matt Laine <matt@brain-fuel.co>
2023-05-15 14:18:23 -0400

    reorganize psalter

---
+++ components/biblescraper/src/org/ecclesiafoundation/clj/core/biblescraper/interface.clj
@@ -0,0 +1,4 @@
+(ns org.ecclesiafoundation.clj.core.biblescraper.interface
+  (:require [clj-http.client :as client]
+            [clojure.pprint :refer pprint]
+            [net.cgrand.enlive-html :as html]))
